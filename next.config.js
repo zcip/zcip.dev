6d768ce6e7dc84937492ee7940b3d022fa3ba112
zcip <zcip.web@gmail.com>
2022-08-09 18:45:37 +0900

    fix: Image Optimization using Next.js' default loader is not compatible with `next export`

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // Error: Image Optimization using Next.js' default loader is not compatible with `next export`.
+  // @link https://nextjs.org/docs/api-reference/next/image#unoptimized
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

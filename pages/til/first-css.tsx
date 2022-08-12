51fd30e7d8d075562b66c0c8ef28b0f670f068ad
zcip <zcip.web@gmail.com>
2022-08-12 15:14:31 +0900

    ifameでCodeSandboxへのプレビューを追加
    https://codesandbox.io/s/epic-galois-92y4iz?file=/src/styles.css

--- pages/til/first-css.tsx
+++ pages/til/first-css.tsx
@@ -2,16 +2,34 @@ import { NextPage } from "next";
 
 const FirstCss: NextPage = () => {
   return (
-    <div>
-      <div className="root">
+    <div className="root">
+      <div className="about">
         ブログっぽいのでマークダウンで書きたいところだが、とりあえずすすめるために新しいページを用意する。
         デフォルトで導入されているので、styled-jsxの書き味をためそうと思う。
         <a href="https://github.com/vercel/styled-jsx">vercel/styled-jsx</a>
       </div>
-
+      <div></div>
+      <iframe
+        src="https://codesandbox.io/embed/epic-galois-92y4iz?fontsize=14&hidenavigation=1&theme=dark"
+        style={{
+          width: "80%",
+          height: "300px",
+          border: 0,
+          borderRadius: "4px",
+          overflow: "hidden",
+        }}
+        title="React"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      ></iframe>
       <style jsx>{`
         .root {
-          margin: 20px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        .about {
+          padding: 20px;
         }
       `}</style>
     </div>

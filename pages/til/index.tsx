1c74b3f144c204938193b07b5051038c49128686
zcip <zcip.web@gmail.com>
2022-08-17 17:24:20 +0900

    listを更新

--- pages/til/index.tsx
+++ pages/til/index.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 
 const TilPage: NextPage = () => {
   return (
-    <div>
+    <div className="flex items-center w-max justify-center flex-col">
+      <h1 className="text-3xl font-bold underline text-center">Hello World</h1>
       <Link href="/til/first-css">はじめてのcss</Link>
       <Link href="/til/first-mdx">はじめてのmdx</Link>
     </div>

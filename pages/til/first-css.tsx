184535447d1bedd4187b48a3fe4dddc53913f252
zcip <zcip.web@gmail.com>
2022-08-19 22:56:54 +0900

    css hover animationを追加

--- pages/til/first-css.tsx
+++ pages/til/first-css.tsx
@@ -8,20 +8,34 @@ const FirstCss: NextPage = () => {
         デフォルトで導入されているので、styled-jsxの書き味をためそうと思う。
         <a href="https://github.com/vercel/styled-jsx">vercel/styled-jsx</a>
       </div>
-      <div></div>
-      <iframe
-        src="https://codesandbox.io/embed/magic-navigation-menu-indicator-92y4iz?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
-        style={{
-          width: "80%",
-          height: "400px",
-          border: 0,
-          borderRadius: "4px",
-          overflow: "hidden",
-        }}
-        title="Magic Navigation Menu Indicator"
-        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-      ></iframe>
+      <div className="iframs">
+        <iframe
+          src="https://codesandbox.io/embed/magic-navigation-menu-indicator-92y4iz?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+          style={{
+            width: "80%",
+            height: "400px",
+            border: 0,
+            borderRadius: "4px",
+            overflow: "hidden",
+          }}
+          title="Magic Navigation Menu Indicator"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
+        <iframe
+          src="https://codesandbox.io/embed/css-icon-hover-jbhb8v?fontsize=14&hidenavigation=1&theme=dark"
+          style={{
+            width: "80%",
+            height: "400px",
+            border: 0,
+            borderRadius: "4px",
+            overflow: "hidden",
+          }}
+          title="css icon hover"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
+      </div>
       <style jsx>{`
         .root {
           display: flex;
@@ -30,6 +44,13 @@ const FirstCss: NextPage = () => {
         }
         .about {
           padding: 20px;
+        }
+        .iframs {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 12px;
+          width: 100%;
         }
       `}</style>
     </div>

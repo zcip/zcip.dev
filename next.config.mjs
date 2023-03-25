60f08312fe0b1d34bc36343f62e515fd31ea0f42
zcip <zcip.web@gmail.com>
2023-03-25 16:24:57 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
+import remarkGfm from "remark-gfm"
+import addMdx from "@next/mdx"
+import rehypePrettyCode from "rehype-pretty-code"
 
 // @see https://nextjs.org/docs/advanced-features/using-mdx
-const withMDX = require("@next/mdx")({
+const withMDX = addMdx({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [],
+    remarkPlugins: [remarkGfm],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
 })
 
-const nextConfig = {
+const nextConfig = withMDX({
   reactStrictMode: true,
   swcMinify: true,
   // @see https://nextjs.org/docs/api-reference/next/image#unoptimized
@@ -20,10 +23,10 @@ const nextConfig = {
   },
   experimental: {
     appDir: true,
-    mdxRs: true,
+    // mdxRs: true,
     typedRoutes: true,
   },
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-}
+})
 
-module.exports = withMDX(nextConfig)
+export default nextConfig

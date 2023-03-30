88319cfad05ecf3e11599038a57cfa7cfdc93e40
zcip <zcip.web@gmail.com>
2023-03-30 11:52:55 +0900

    react-tweetでNextのコンポーネントを使う

--- components/MdxComponents.tsx
+++ components/MdxComponents.tsx
@@ -1,9 +1,15 @@
-import { Tweet, type TweetProps } from "react-tweet"
+import { Tweet, type TweetProps, type TweetComponents } from "react-tweet"
+import Image from "next/image"
+
+const tweetComponents: TweetComponents = {
+  AvatarImg: (props) => <Image {...props} />,
+  MediaImg: (props) => <Image {...props} fill unoptimized />,
+}
 
 export const components = {
   Tweet: (props: TweetProps) => (
     <div className="not-prose">
-      <Tweet {...props} />
+      <Tweet {...props} components={tweetComponents} />
     </div>
   ),
 }

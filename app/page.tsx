8d61144f9f3fb9fcdbee8d39038de36b1d5cff9b
zcip <zcip.web@gmail.com>
2023-04-03 01:31:05 +0900

    Update top page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,37 @@
 import Link from "next/link"
+import { fetchBlogs } from "./blog/lib"
+import { formatJP } from "@/lib/date"
 
-export default function Home() {
+export default async function HomePage() {
+  const blogs = await fetchBlogs()
   return (
-    <>
-      <div className="px-2">
-        <main className="flex flex-col justify-center items-center min-h-screen flex-1">
-          <Link href="/about">About me</Link>
-          <Link href="/til">TODAY I Lerned</Link>
-          <Link href="/typescript">TypeScript</Link>
-        </main>
+    <main className="flex flex-col text-gray-300 px-2">
+      <div className="pb-20">
+        <Link
+          href="/blog"
+          className="text-2xl mb-10 hover:border-b-2 border-pink-700"
+        >
+          <h4 className="text-white">最近のブログ記事</h4>
+        </Link>
+        <ul className="mb-6">
+          {blogs.slice(0, 3).map((blog) => (
+            <li key={blog.slug} className="p-6 block text-gray-300">
+              <h4 className="mb-4 text-gray-500">
+                {formatJP(blog.frontmatter.date)}
+              </h4>
+              <Link
+                href={`/blog/${blog.slug}`}
+                className="text-xl hover:border-b-2 hover:text-white border-pink-700 text-gray-200"
+              >
+                {blog.frontmatter.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Link href="/blog" className="text-xl mb-10 text-center">
+          <h4 className="text-gray-300 hover:text-white">すべての記事を見る</h4>
+        </Link>
       </div>
-    </>
+    </main>
   )
 }

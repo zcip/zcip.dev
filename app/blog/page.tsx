5358a984f584c6e67c16d5b8710b150e665fc97b
zcip <zcip.web@gmail.com>
2023-04-02 19:29:10 +0900

    design: blog pageのデザイン

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import fs from "fs/promises"
 import { getBlogDirpath, getCachedBlogContent } from "./misc"
+import { format } from "date-fns"
 
 export const metadata = {
   title: "Blog - zcip",
@@ -15,15 +16,23 @@ export default async function BlogPage() {
         slug: name,
       }))
     )
-  ).sort((a, b) => Number(a.frontmatter.date) - Number(b.frontmatter.date))
+  ).sort((a, b) => Number(b.frontmatter.date) - Number(a.frontmatter.date))
 
   return (
-    <div className="px-2">
-      <main className="flex flex-col justify-center items-center min-h-screen flex-1">
-        <ul>
+    <div className="px-2 w-full">
+      <main className="">
+        <ul className="flex flex-col gap-2">
           {blogs?.map((blog) => (
-            <li key={blog.frontmatter.title}>
-              <Link href={`/blog/${blog.slug}`}>{blog.frontmatter.title}</Link>
+            <li key={blog.frontmatter.title} className="block p-6 rounded-lg">
+              <h4 className="text-gray-500 mb-4">
+                {format(new Date(blog.frontmatter.date), "yyyy年M月d日")}
+              </h4>
+              <Link
+                href={`/blog/${blog.slug}`}
+                className="text-2xl text-gray-100 hover:border-b-2 border-pink-600"
+              >
+                {blog.frontmatter.title}
+              </Link>
             </li>
           ))}
         </ul>
